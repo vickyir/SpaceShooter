f9b8410c16fad5f8a4fb28a93f0e8a3a7cbf40c3
VickyIr <vicky.ir@students.amikom.ac.id>
2023-05-24 04:05:50 +0700

    Initial Commit

---
+++ SpaceShooter/SpaceShooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceShooterApp.swift
+//  SpaceShooter
+//
+//  Created by Vicky Irwanto on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceShooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
